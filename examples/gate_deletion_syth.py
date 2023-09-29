11e09f711f1b99cd38ec6f4bcaf539b4b0e4dc43
Alon <alon.kukliansky.is@npd.edu>
2023-09-28 22:43:33 -0700

    Some more pre-commit fixes

--- examples/gate_deletion_syth.py
+++ examples/gate_deletion_syth.py
@@ -18,104 +18,99 @@ from bqskit.passes import UnfoldPass
 from bqskitqfactorjax.qfactor_jax import QFactor_jax
 
 
-
 def run_gate_del_flow_example():
-        # The circuit to resynthesize
-        file_path = os.path.dirname(__file__) + '/grover5.qasm'
+    # The circuit to resynthesize
+    file_path = os.path.dirname(__file__) + '/grover5.qasm'
 
-        # Compiler runtime configuration
-        amount_of_workers = 10
+    # Compiler runtime configuration
+    amount_of_workers = 10
 
-        # Set the size of paritions
-        partition_size = 4
+    # Set the size of paritions
+    partition_size = 4
 
-        # QFactor hyperparameters -
-        # see intantiation example for more detiles on the parameters
-        num_multistarts = 32
-        max_iters = 100000
-        min_iters = 3
-        diff_tol_r = 1e-5
-        diff_tol_a = 0.0
-        dist_tol = 1e-10
+    # QFactor hyperparameters -
+    # see intantiation example for more detiles on the parameters
+    num_multistarts = 32
+    max_iters = 100000
+    min_iters = 3
+    diff_tol_r = 1e-5
+    diff_tol_a = 0.0
+    dist_tol = 1e-10
 
-        diff_tol_step_r = 0.1
-        diff_tol_step = 200
+    diff_tol_step_r = 0.1
+    diff_tol_step = 200
 
-        beta = 0
+    beta = 0
 
+    print(f'Will compile {file_path}')
 
+    # Read the QASM circuit
+    in_circuit = Circuit.from_file(file_path)
 
-        print(f'Will compile {file_path}')
+    # Preoare the instantiator
+    batched_instantiation = QFactor_jax(
+        diff_tol_r=diff_tol_r,
+        diff_tol_a=diff_tol_a,
+        min_iters=min_iters,
+        max_iters=max_iters,
+        dist_tol=dist_tol,
+        diff_tol_step_r=diff_tol_step_r,
+        diff_tol_step=diff_tol_step,
+        beta=beta,
+    )
+    instantiate_options = {
+        'method': batched_instantiation,
+        'multistarts': num_multistarts,
+    }
 
-        # Read the QASM circuit
-        in_circuit = Circuit.from_file(file_path)
+    # Prepare the comiplation passes
+    passes = [
+        # Convert U3's to VU
+        ToVariablePass(),
 
-        # Preoare the instantiator
-        batched_instantiation = QFactor_jax(
-                                        diff_tol_r=diff_tol_r,
-                                        diff_tol_a=diff_tol_a,
-                                        min_iters=min_iters,
-                                        max_iters=max_iters,
-                                        dist_tol=dist_tol,
-                                        diff_tol_step_r=diff_tol_step_r,
-                                        diff_tol_step = diff_tol_step,
-                                        beta=beta,
-        )
-        instantiate_options={
-                'method': batched_instantiation,
-                'multistarts': num_multistarts,
-        }
+        # Split the circuit into partitions
+        QuickPartitioner(partition_size),
 
+        # For each partition perform scanning gate removal using QFactor jax
+        ForEachBlockPass([
+            ScanningGateRemovalPass(
+                instantiate_options=instantiate_options,
+            ),
+        ]),
 
-        # Prepare the comiplation passes
-        passes =[
-                # Convert U3's to VU
-                ToVariablePass(),
+        # Combine the partitions back into a circuit
+        UnfoldPass(),
 
-                # Split the circuit into partitions
-                QuickPartitioner(partition_size),
+        # Convert back the VariablueUnitaires into U3s
+        ToU3Pass(),
+    ]
 
-                # For each partition perform scanning gate removal using QFactor jax
-                ForEachBlockPass([
-                        ScanningGateRemovalPass(instantiate_options=instantiate_options),
-                ]),
+    # Create the compilation task
 
-                # Combine the partitions back into a circuit
-                UnfoldPass(),
+    with Compiler(
+            num_workers=amount_of_workers,
+            runtime_log_level=logging.INFO,
+    ) as compiler:
 
-                # Convert back the VariablueUnitaires into U3s
-                ToU3Pass(),
-        ]
+        print(f'Starting gate deletion flow using Qfactor JAX')
+        start = timer()
+        out_circuit = compiler.compile(in_circuit, passes)
+        end = timer()
+        run_time = end - start
 
-
-        # Create the compilation task
-
-
-        with Compiler(
-                num_workers=amount_of_workers,
-                runtime_log_level=logging.INFO,
-        ) as compiler:
-
-                print(f'Starting gate deletion flow using Qfactor JAX')
-                start = timer()
-                out_circuit = compiler.compile(in_circuit, passes)
-                end = timer()
-                run_time = end - start
-
-
-        return in_circuit, out_circuit, run_time
+    return in_circuit, out_circuit, run_time
 
 
 if __name__ == '__main__':
 
-        in_circuit, out_circuit, run_time = run_gate_del_flow_example()
+    in_circuit, out_circuit, run_time = run_gate_del_flow_example()
 
-        print(
-                f'Partitioning + Synthesis took {run_time}'
-                f'seconds using Qfactor JAX instantiation method.',
-        )
+    print(
+        f'Partitioning + Synthesis took {run_time}'
+        f'seconds using Qfactor JAX instantiation method.',
+    )
 
-        print(
-                f'Circuit finished with gates: {out_circuit.gate_counts}, '
-                f'while started with {in_circuit.gate_counts}',
-        )
+    print(
+        f'Circuit finished with gates: {out_circuit.gate_counts}, '
+        f'while started with {in_circuit.gate_counts}',
+    )
--- examples/toffoli_instantiation.py
+++ examples/toffoli_instantiation.py
@@ -14,30 +14,28 @@ from bqskit.qis.unitary import UnitaryMatrix
 from bqskitqfactorjax.qfactor_jax import QFactor_jax
 
 
-def run_toffoli_instantiation(dist_tol_requested = 1e-10):
+def run_toffoli_instantiation(dist_tol_requested=1e-10):
     qfactr_gpu_instantiator = QFactor_jax(
 
-            dist_tol  = dist_tol_requested,       # Stopping criteria for distance
+        dist_tol=dist_tol_requested,       # Stopping criteria for distance
 
-            max_iters = 100000,      # Maximum number of iterations
-            min_iters = 10,          # Minimum number of iterations
+        max_iters=100000,      # Maximum number of iterations
+        min_iters=10,          # Minimum number of iterations
 
-            #One step plateau detection -
-            #diff_tol_a + diff_tol_r ∗ |c(i)| <= |c(i)|-|c(i-1)|
-            diff_tol_a = 0.0,       # Stopping criteria for distance change
-            diff_tol_r = 1e-10,     # Relative criteria for distance change
+        # One step plateau detection -
+        # diff_tol_a + diff_tol_r ∗ |c(i)| <= |c(i)|-|c(i-1)|
+        diff_tol_a=0.0,       # Stopping criteria for distance change
+        diff_tol_r=1e-10,     # Relative criteria for distance change
 
-            #Long plateau detection -
-            # diff_tol_step_r*|c(i-diff_tol_step)| <= |c(i)|-|c(i-diff_tol_step)|
-            diff_tol_step_r = 0.1, #The relative improvment expected
-            diff_tol_step   = 200,   #The interval in which to check the improvment
+        # Long plateau detection -
+        # diff_tol_step_r*|c(i-diff_tol_step)| <= |c(i)|-|c(i-diff_tol_step)|
+        diff_tol_step_r=0.1,  # The relative improvment expected
+        diff_tol_step=200,  # The interval in which to check the improvment
 
-            #Regularization parameter - [0.0 - 1.0]
-            # Increase to overcome local minimumas at the price of longer compute
-            beta = 0.0,
+        # Regularization parameter - [0.0 - 1.0]
+        # Increase to overcome local minimumas at the price of longer compute
+        beta=0.0,
     )
-
-
 
     # We will optimize towards the toffoli unitary.
     toffoli = np.array([
@@ -60,17 +58,17 @@ def run_toffoli_instantiation(dist_tol_requested = 1e-10):
     circuit.append_gate(VariableUnitaryGate(2), [0, 2])
     circuit.append_gate(VariableUnitaryGate(2), [0, 1])
 
-
     # Instantiate the circuit template with qfactor
     circuit.instantiate(
         toffoli,
-        multistarts = 16,
+        multistarts=16,
         method=qfactr_gpu_instantiator,
     )
 
     # Calculate and print final distance
     dist = circuit.get_unitary().get_distance_from(toffoli, 1)
     return dist
+
 
 if __name__ == '__main__':
     dist = run_toffoli_instantiation()
