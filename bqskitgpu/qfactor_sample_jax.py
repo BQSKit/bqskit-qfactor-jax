9f413c984e4a37c423e1ee46ad5f8d5a5cb4e004
Alon Kukliansky <alonkukl@gmail.com>
2023-10-02 22:17:30 -0700

    Before pre-commits fixs

--- bqskitgpu/qfactor_sample_jax.py
+++ bqskitgpu/qfactor_sample_jax.py
@@ -1,33 +1,34 @@
 from __future__ import annotations
-import functools
+
 import logging
 import os
-from typing import TYPE_CHECKING, Sequence
-
-
-import numpy as np
-import numpy.typing as npt
-from scipy.stats import unitary_group
-import matplotlib.pyplot as plt
+from typing import TYPE_CHECKING
 
 import jax
 import jax.numpy as jnp
+import jax.numpy.logical_and as jand
+import jax.numpy.logical_not as jnot
+import jax.numpy.logical_or as jor
+import numpy as np
+import numpy.typing as npt
+from bqskit.ir import CircuitLocation
+from bqskit.ir import Gate
+from bqskit.ir.gates import ConstantGate
+from bqskit.ir.gates import VariableUnitaryGate
+from bqskit.ir.opt import Instantiater
+from bqskit.qis import UnitaryMatrix
+from bqskit.qis.state import StateSystem
+from bqskit.qis.state import StateVector
+from jax import Array
+from scipy.stats import unitary_group
 
-from bqskit.ir.gates.constantgate import ConstantGate
-from bqskit.ir.gates.parameterized.u3 import U3Gate
-from bqskit.ir.gates.parameterized.unitary import VariableUnitaryGate
-from bqskit.qis.unitary.unitarymatrix import UnitaryMatrix
-from bqskit.ir.opt.instantiater import Instantiater
-from bqskit.ir.location import CircuitLocationLike
-from bqskit.ir.location import CircuitLocation
-from bqskit.qis.state.state import StateVector
-from bqskit.qis.state.system import StateSystem
+from bqskitgpu.qfactor_jax import _apply_padding_and_flatten
+from bqskitgpu.qfactor_jax import _remove_padding_and_create_matrix
+from bqskitgpu.singlelegedtensor import LHSTensor
+from bqskitgpu.singlelegedtensor import RHSTensor
+from bqskitgpu.singlelegedtensor import SingleLegSideTensor
 from bqskitgpu.unitary_acc import VariableUnitaryGateAcc
-from bqskitgpu.unitarybuilderjax import UnitaryBuilderJax
 from bqskitgpu.unitarymatrixjax import UnitaryMatrixJax
-from bqskitgpu.singlelegedtensor import SingleLegSideTensor, RHSTensor, LHSTensor
-
-from bqskit.ir.gates import  CXGate
 
 if TYPE_CHECKING:
     from bqskit.ir.circuit import Circuit
@@ -40,19 +41,18 @@ _logger = logging.getLogger(__name__)
 jax.config.update('jax_enable_x64', True)
 
 
-
 class QFactor_sample_jax(Instantiater):
 
-
     def __init__(
-            self,
-            dist_tol: float = 1e-10,
-            max_iters: int = 100000,
-            min_iters: int = 1000,
-            beta: float = 0.0,
-            amount_of_validation_states : int = 2,
-        ):
-
+        self,
+        dist_tol: float = 1e-10,
+        max_iters: int = 100000,
+        min_iters: int = 1000,
+        beta: float = 0.0,
+        amount_of_validation_states: int = 2,
+        num_params_coeff: float = 1,
+        overtrain_ratio: float = 1 / 32,
+    ):
 
         if not isinstance(dist_tol, float) or dist_tol > 0.5:
             raise TypeError('Invalid distance threshold.')
@@ -63,21 +63,21 @@ class QFactor_sample_jax(Instantiater):
         if not isinstance(min_iters, int) or min_iters < 0:
             raise TypeError('Invalid minimum number of iterations.')
 
-
         self.dist_tol = dist_tol
         self.max_iters = max_iters
         self.min_iters = min_iters
 
         self.beta = beta
         self.amount_of_validation_states = amount_of_validation_states
-
+        self.num_params_coeff = num_params_coeff
+        self.overtrain_ratio = overtrain_ratio
 
     def instantiate(
-            self,
-            circuit: Circuit,
-            target: UnitaryMatrix | StateVector | StateSystem,
-            x0: npt.NDArray[np.float64],
-        ) -> npt.NDArray[np.float64]:
+        self,
+        circuit: Circuit,
+        target: UnitaryMatrix | StateVector | StateSystem,
+        x0: npt.NDArray[np.float64],
+    ) -> npt.NDArray[np.float64]:
 
         return self.multi_start_instantiate(circuit, target, 1)
 
@@ -100,18 +100,14 @@ class QFactor_sample_jax(Instantiater):
         params = self.multi_start_instantiate(circuit, target, num_starts)
         circuit.set_params(params)
 
-
     async def multi_start_instantiate_async(
         self,
         circuit: Circuit,
         target: UnitaryLike | StateLike | StateSystemLike,
         num_starts: int,
     ) -> npt.NDArray[np.float64]:
-        
+
         return self.multi_start_instantiate(circuit, target, num_starts)
-    
-
-
 
     def multi_start_instantiate(
         self,
@@ -120,7 +116,7 @@ class QFactor_sample_jax(Instantiater):
         num_starts: int,
     ) -> npt.NDArray[np.float64]:
         if len(circuit) == 0:
-            return []
+            return np.array([])
 
         circuit = circuit.copy()
 
@@ -130,10 +126,11 @@ class QFactor_sample_jax(Instantiater):
             if isinstance(g, VariableUnitaryGate):
                 g.__class__ = VariableUnitaryGateAcc
 
-
         target = UnitaryMatrixJax(self.check_target(target))
         locations = tuple([op.location for op in circuit])
         gates = tuple([op.gate for op in circuit])
+        biggest_gate_size = max(gate.num_qudits for gate in gates)
+
         untrys = []
 
         for gate in gates:
@@ -150,142 +147,75 @@ class QFactor_sample_jax(Instantiater):
                     _ in range(num_starts)
                 ]
 
-            untrys.append(pre_padding_untrys)
+            untrys.append([
+                _apply_padding_and_flatten(
+                    pre_padd, gate, biggest_gate_size,
+                ) for pre_padd in pre_padding_untrys
+            ])
 
         untrys = jnp.array(np.stack(untrys, axis=1))
 
+        amount_of_trainng_states = 0
+        for g in gates:
+            amount_of_trainng_states += self.num_params_coeff * g.num_params
 
+        amount_of_trainng_states = np.round(amount_of_trainng_states)
 
-    def state_sample_sweep(N:int, target:UnitaryMatrixJax, locations, gates, untrys, num_qudits, radixes,  dist_tol, max_iters:int = 1000, beta:float=0.0, training_states_kets = None, validation_states_kets = None):
+        training_states_kets = self.generate_random_states(
+            amount_of_trainng_states,
+        )
+        validation_states_kets = self.generate_random_states(
+            self.amount_of_validation_states,
+        )
 
+        final_untrys, training_costs, validation_costs, iteration_counts = _jited_loop_vmaped_state_sample_sweep(
+            target, locations, gates, untrys,
+            self.dist_tol, self.max_iters, self.beta,
+            training_states_kets, validation_states_kets,
+        )
 
-        training_costs = []
-        validation_costs = []
+        it = iteration_counts[0]
+        untrys = final_untrys
+        best_start = jnp.argmin(training_costs)
 
-        amount_of_gates = len(gates)
-
-        training_states_bras = []
-        if training_states_kets == None:
-            training_states_kets = QFactor_sample_jax.generate_random_states(N, np.prod(radixes))
+        if any(training_costs < self.dist_tol):
+            _logger.debug(
+                f'Terminated: {it} c1 = {training_costs} <= dist_tol.\n'
+                f'Best start is {best_start}',
+            )
+        elif all(validation_costs > self.overtrain_ratio * training_costs):
+            _logger.debug(
+                f'Terminated: {it} overtraining detected in all multistarts',
+            )
+        elif it >= self.max_iters:
+            _logger.debug('Terminated: iteration limit reached.')
         else:
-            assert N==len(training_states_kets)
-
-        for ket in training_states_kets:
-            training_states_bras.append(ket.T.conj())
-
-        validation_states_bras = []
-        if validation_states_kets == None:
-            validation_states_kets = QFactor_sample_jax.generate_random_states(N//4, np.prod(radixes))    
-
-        for ket in validation_states_kets:
-            validation_states_bras.append(ket.T.conj())
-
-
-        
-
-        #### Compute As
-        target_dagger = target.T.conj()
-        A_train = RHSTensor(list_of_states = training_states_bras, num_qudits=num_qudits, radixes=radixes)
-        A_train.apply_left(target_dagger, range(num_qudits))
-
-        A_val = RHSTensor(list_of_states = validation_states_bras, num_qudits=num_qudits, radixes=radixes)
-        A_val.apply_left(target_dagger, range(num_qudits))
-
-        B0_train = LHSTensor(list_of_states = training_states_kets, num_qudits=num_qudits, radixes=radixes)
-        B0_val = LHSTensor(list_of_states = validation_states_kets, num_qudits=num_qudits, radixes=radixes)
-
-        ### Until done....
-        it = 1
-        while True:
-
-            ### Compute B
-            
-            B = [B0_train]
-            for location, utry in zip(locations[:-1], untrys[:-1]):
-                B.append(B[-1].copy())
-                B[-1].apply_right(utry, location)
-
-            temp = B[-1].copy()
-            temp.apply_right(untrys[-1], locations[-1])
-            training_cost = 2*(1-jnp.real(SingleLegSideTensor.calc_env(temp, A_train, [])[0])/N)
-            # print(f'initial {cost = }')
-
-            ### iterate over every gate from right to left and update it
-            new_untrys = [None]*amount_of_gates
-            a_train:RHSTensor = A_train.copy()
-            a_val:RHSTensor = A_val.copy()
-            for idx in reversed(range(amount_of_gates)):
-                b = B[idx]
-                gate = gates[idx]
-                location = locations[idx]
-                utry = untrys[idx]
-                if gate.num_params > 0:
-                    # print(f"Updating {utry._utry = }")
-                    env = SingleLegSideTensor.calc_env(b, a_train, location)
-                    utry = gate.optimize(env.T, get_untry=True, prev_utry=utry, beta=beta)
-                    # print(f"to {utry._utry = }")
-                
-                new_untrys[idx] = utry
-                a_train.apply_left(utry, location)
-                a_val.apply_left(utry, location)
-
-            untrys = new_untrys
-
-
-            training_cost = 2*(1-jnp.real(SingleLegSideTensor.calc_env(B0_train, a_train, [])[0])/N)
-            validation_cost = 2*(1-jnp.real(SingleLegSideTensor.calc_env(B0_val, a_val, [])[0])/(N//4))
-
-            training_costs.append(training_cost)
-            validation_costs.append(validation_cost)
-            
-            if it%100 == 0:
-                print(f'{it = } {training_cost = }')
-                print(f'{it = } {validation_cost = }')
-
-            if it > 10 and validation_cost/32 > training_cost:
-                print("Stopped due to no improvment in validation set")
-                break
-            if training_cost <= dist_tol or it > 3000:
-                print(f'{it = } {training_cost = }')
-                break
-            it+=1
-
-
-
-        plt.figure(figsize=(10, 6))
-        plt.plot(training_costs, label='Training Costs', marker='o')
-        plt.plot(validation_costs, label='Validation Costs', marker='x')
-
-        # Add labels and title
-        plt.xlabel('Epochs')
-        plt.ylabel('Cost')
-        plt.yscale('log')
-        plt.title('Training and Validation Costs')
-        plt.legend()
-
-        # Show the plot
-        plt.grid(True)
-        plt.show()
+            _logger.error(
+                f'Terminated with no good reason after {it} iterstion '
+                f'with c1s {training_costs}.',
+            )
 
         params = []
-        for untry, gate in zip(untrys, gates):
-            if  isinstance(gate, ConstantGate):
+        for untry, gate in zip(untrys[best_start], gates):
+            if isinstance(gate, ConstantGate):
                 params.extend([])
-            else:    
+            else:
                 params.extend(
-                    gate.get_params(untry.numpy),
-            )
-            
+                    gate.get_params(
+                        _remove_padding_and_create_matrix(untry, gate),
+                    ),
+                )
+
         return np.array(params)
 
-
-
-
-
     @staticmethod
-    def generate_random_states(amount_of_states, size_of_state):
+    def generate_random_states(
+        amount_of_states: int,
+        size_of_state: int,
+    ) -> list[npt.NDArray]:
         """
-        Generate a list of random state vectors (kets) using random unitary matrices.
+        Generate a list of random state vectors (kets) using random unitary
+        matrices.
 
         This function generates a specified number of random quantum state vectors
         (kets) by creating random unitary matrices and extracting their first columns.
@@ -305,6 +235,207 @@ class QFactor_sample_jax(Instantiater):
             rand_unitary = unitary_group.rvs(size_of_state)
             states_to_add_in_step = min(states_to_add, size_of_state)
             for i in range(states_to_add_in_step):
-                states_kets.append(rand_unitary[:, i:i+1])
+                states_kets.append(rand_unitary[:, i:i + 1])
             states_to_add -= states_to_add_in_step
         return states_kets
+
+
+def _loop_vmaped_state_sample_sweep(
+    target: UnitaryMatrixJax, num_qudits: int, radixes: tuple[int, ...],
+    locations: tuple[CircuitLocation, ...],
+    gates: tuple[Gate, ...], untrys: Array,
+    dist_tol: float, max_iters: int, beta: float,
+    amount_of_starts: int, min_iters: int,
+    overtrian_ratio: float,
+    training_states_kets: Array,
+    validation_states_kets: Array,
+) -> tuple[Array, Array[float], Array[float], Array[int]]:
+
+    validation_states_bras = jax.vmap(
+        lambda ket: ket.T.conj(),
+    )(validation_states_kets)
+
+    training_states_bras = jax.vmap(
+        lambda ket: ket.T.conj(),
+    )(training_states_kets)
+
+    # Calculate the A and B0 tensor
+    target_dagger = target.T.conj()
+    A_train = RHSTensor(
+        list_of_states=training_states_bras,
+        num_qudits=num_qudits, radixes=radixes,
+    )
+    A_train.apply_left(target_dagger, range(num_qudits))
+
+    A_val = RHSTensor(
+        list_of_states=validation_states_bras,
+        num_qudits=num_qudits, radixes=radixes,
+    )
+    A_val.apply_left(target_dagger, range(num_qudits))
+
+    B0_train = LHSTensor(
+        list_of_states=training_states_kets,
+        num_qudits=num_qudits, radixes=radixes,
+    )
+    B0_val = LHSTensor(
+        list_of_states=validation_states_kets,
+        num_qudits=num_qudits, radixes=radixes,
+    )
+
+    # In JAX the body of a while must be a function that accepet and returns
+    # the same type, and also the check should be a function that accepts it
+    # and return a boolean
+
+    def should_continue(loop_var: tuple):
+        _, training_costs, validation_costs, iteration_counts = loop_var
+
+        any_reached_required_tol = jnp.any(
+            jax.vmap(
+                lambda cost: cost <= dist_tol,
+                training_costs,
+            ),
+        )
+        reached_max_iteration = iteration_counts[0] > max_iters
+
+        above_min_iteration = iteration_counts[0] > min_iters
+
+        any_reached_over_training = jnp.any(
+            jax.vmap(
+                lambda train_cost, val_cost:
+                val_cost > overtrian_ratio * train_cost,
+                (training_costs, validation_costs),
+            ),
+        )
+
+        return jnot(
+            jor(
+                any_reached_required_tol,
+                jor(
+                    reached_max_iteration,
+                    jand(
+                        above_min_iteration,
+                        any_reached_over_training,
+                    ),
+                ),
+            ),
+        )
+
+    def _while_body_to_be_vmaped(loop_var: tuple):
+
+        untrys, training_cost, validation_cost, iteration_count = loop_var
+
+        untrys_as_matrixs: list[UnitaryMatrixJax] = []
+        for gate_index, gate in enumerate(gates):
+            untrys_as_matrixs.append(
+                UnitaryMatrixJax(
+                    _remove_padding_and_create_matrix(
+                        untrys[gate_index], gate,
+                    ), gate.radixes,
+                ),
+            )
+
+        untrys_as_matrixs, training_cost, validation_cost = state_sample_single_sweep(
+            locations, gates, untrys_as_matrixs,
+            beta, A_train, A_val, B0_train, B0_val,
+        )
+
+        iteration_count += 1
+
+        biggest_gate_size = max(gate.num_qudits for gate in gates)
+        final_untrys_padded = jnp.array([
+            _apply_padding_and_flatten(
+                untry.numpy.flatten(
+                ), gate, biggest_gate_size,
+            ) for untry, gate in zip(untrys, gates)
+        ])
+
+        return (
+            final_untrys_padded, training_cost, validation_cost,
+            iteration_count,
+        )
+
+    while_body_vmaped = jax.vmap(_while_body_to_be_vmaped)
+
+    initial_loop_var = (
+        untrys,
+        jnp.ones(amount_of_starts),  # train_cost
+        jnp.ones(amount_of_starts),  # val_cost
+        jnp.zeros(amount_of_starts),  # iter_count
+    )
+
+    r = jax.lax.while_loop(should_continue, while_body_vmaped, initial_loop_var)
+    final_untrys, training_costs, validation_costs, iteration_counts = r
+
+    return final_untrys, training_costs, validation_costs, iteration_counts
+
+
+def state_sample_single_sweep(
+    locations: tuple[CircuitLocation, ...],
+    gates: tuple[Gate, ...],
+    untrys: list[UnitaryMatrixJax], beta: float,
+    A_train: RHSTensor, A_val: RHSTensor,
+    B0_train: LHSTensor, B0_val: LHSTensor,
+) -> list[UnitaryMatrixJax] | float | float:
+
+    amount_of_gates = len(gates)
+    B = [B0_train]
+    for location, utry in zip(locations[:-1], untrys[:-1]):
+        B.append(B[-1].copy())
+        B[-1].apply_right(utry, location)
+
+    temp = B[-1].copy()
+    temp.apply_right(untrys[-1], locations[-1])
+    training_cost = 2 * (
+        1 - jnp.real(
+            SingleLegSideTensor.calc_env(
+                temp,
+                A_train, [],
+            )[0],
+        ) / A_train.single_leg_radix
+    )
+
+    # iterate over every gate from right to left and update it
+    new_untrys = [None] * amount_of_gates
+    a_train: RHSTensor = A_train.copy()
+    a_val: RHSTensor = A_val.copy()
+    for idx in reversed(range(amount_of_gates)):
+        b = B[idx]
+        gate = gates[idx]
+        location = locations[idx]
+        utry = untrys[idx]
+        if gate.num_params > 0:
+            env = SingleLegSideTensor.calc_env(b, a_train, location)
+            utry = gate.optimize(
+                env.T, get_untry=True,
+                prev_utry=utry, beta=beta,
+            )
+
+        new_untrys[idx] = utry
+        a_train.apply_left(utry, location)
+        a_val.apply_left(utry, location)
+
+    untrys = new_untrys
+
+    training_cost = calc_cost(A_train, B0_train, a_train)
+
+    validation_cost = calc_cost(A_val, B0_val, a_val)
+
+    return untrys, training_cost, validation_cost
+
+
+def calc_cost(A: RHSTensor, B0: LHSTensor, a: RHSTensor) -> float:
+    return 2 * (
+        1 - jnp.real(
+            SingleLegSideTensor.calc_env(B0, a, [])[0],
+        ) / A.single_leg_radix
+    )
+
+
+if 'NO_JIT_QFACTOR' in os.environ:
+    _jited_loop_vmaped_state_sample_sweep = _loop_vmaped_state_sample_sweep
+else:
+    _jited_loop_vmaped_state_sample_sweep = jax.jit(
+        _loop_vmaped_state_sample_sweep, static_argnums=(
+            1, 2, 3, 4, 6, 7, 8, 9, 10,
+        ),
+    )
